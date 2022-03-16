eaf47d0ee8d078da82f268c44cd4a0eb7199a9ae
Tomas Pospisek <tpo@sourcepole.ch>
2022-03-16 16:47:14 +0100

    allow to pass the alembic base dir as parameter
    The reason for changing directories is that we want also to run the same
    migration script as a /docker-entrypoint-initdb.d/ script when starting
    the postgres docker image. In that case the run-migrations.sh script needs
    to know where the alembic migration setup is.

--- run-migrations.sh
+++ run-migrations.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+# we expect the alembic setup to reside in
+# /root/alembic by default. So the default base dir
+# would be /root
+BASE_OF_ALEMBIC_MIGRATIONS="${BASE_OF_ALEMBIC_MIGRATIONS:-/root}"
+cd "$BASE_OF_ALEMBIC_MIGRATIONS"
+
 # run migrations
 PGSERVICE=qwc_configdb alembic upgrade $ALEMBIC_VERSION
