693d7fedcfd2cf186c7c78ec92b9379541fd2a71
Tomas Pospisek <tpo@sourcepole.ch>
2022-05-20 22:47:31 +0200

    use qwc_admin user configured in /root/.pg_service.conf

--- setup-qwc_config-schema.sh
+++ setup-qwc_config-schema.sh
@@ -5,7 +5,10 @@ set -e
 #  CREATE ROLE qwc_admin LOGIN PASSWORD 'qwc_admin';
 # EOSQL
 
+# get admin user
+QWC_ADMIN_USER=$( grep ^user= /root/.pg_service.conf | sed 's/user=//' )
+
 psql service=qwc_configdb <<-EOSQL
-  CREATE SCHEMA qwc_config AUTHORIZATION qwc_admin;
+  CREATE SCHEMA qwc_config AUTHORIZATION "$QWC_ADMIN_USER";
   COMMENT ON SCHEMA qwc_config IS 'Configurations for qwc-services';
 EOSQL
